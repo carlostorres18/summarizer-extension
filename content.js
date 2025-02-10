1ba4c390ea9a8f5c9defdb06cbb3081075eb9cda
carlostorres18 <torreccarlos@gmail.com>
2025-02-10 10:46:27 -0600

    Added server and edited manifest (spelling mistake).

--- content.js
+++ content.js
@@ -1,14 +1,5 @@
-function extractMainText(){
-    let paragraphs = document.querySelector("p");
-    let text = "";
-    paragraphs.forEach(p => text += p.innerText + " ");
-    return text.trim();
-}
-
-// This will listen to messages from popup.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "extract_text"){
-        let textContent = extractMainText();
-        sendResponse({text: textContent});
+    if (request.action === "extractText"){
+        sendResponse({text: document.body.innerText});
     }
 });
